12aa13a7da710b7562df1775af3cbf14dcc54223
Dmitry Voytik <voytikd@gmail.com>
2019-09-05 11:50:40 +0200

    add fish shell config
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

---
+++ dotfiles/config.fish
@@ -0,0 +1,15 @@
+
+set -x PATH $PATH ~/.bin/
+
+export LC_ALL=en_US.UTF-8
+
+#
+# common aliases
+#
+alias r='vim -R -'
+
+alias gdc='clear; git diff --cached'
+alias gd='clear; git diff'
+alias gst='clear; git status'
+alias ga='git add'
+alias gam='git commit --ammend'
