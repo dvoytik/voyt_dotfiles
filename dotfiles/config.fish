390366b6d7ee44d64ff9dd54e09a7095e4b764e6
Dmitry Voytik <voytikd@gmail.com>
2022-12-11 19:14:42 +0100

    nvim: add telescope key bindings
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -2,6 +2,7 @@
 set -x PATH /usr/local/bin $PATH
 set -x PATH $PATH ~/.bin/
 set -x PATH $PATH ~/.cargo/bin/
+set -x PATH $PATH ~/.bin/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-apple-darwin/bin
 
 export LC_ALL=en_US.UTF-8
 
@@ -18,3 +19,5 @@ alias gam='git commit --ammend'
 alias gap='clear; git add -p'
 
 alias t='tree . | vim -'
+
+alias llt="ls -ltrh"
--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -192,3 +192,10 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = format_sync_grp,
 })
+
+-- Telescope hot keys
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', ',ff', builtin.find_files, {})
+vim.keymap.set('n', ',fg', builtin.live_grep, {})
+vim.keymap.set('n', ',fb', builtin.buffers, {})
+vim.keymap.set('n', ',fh', builtin.help_tags, {})
