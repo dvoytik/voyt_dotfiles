af24df0cc36a1ccfc669e9d84cd4b5bbca22bcb5
Dmitry Voytik <voytikd@gmail.com>
2023-01-01 12:13:45 +0100

    first astronvim config update
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- dotfiles/astronvim_user/init.lua
+++ dotfiles/astronvim_user/init.lua
@@ -9,7 +9,7 @@ local config = {
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
-    channel = "nightly", -- "stable" or "nightly"
+    channel = "stable", -- "stable" or "nightly"
     version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
     branch = "main", -- branch name (NIGHTLY ONLY)
     commit = nil, -- commit hash (NIGHTLY ONLY)
@@ -46,7 +46,7 @@ local config = {
       number = true, -- sets vim.opt.number
       spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = false, -- sets vim.opt.wrap
+      wrap = true, -- sets vim.opt.wrap
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
