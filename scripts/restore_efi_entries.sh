bc4886dc4348de48c28e8f21416d69c7ab943c9f
Dmitry Voytik <voytikd@gmail.com>
2023-12-16 21:05:00 +0100

    fix efi restore script
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- scripts/restore_efi_entries.sh
+++ scripts/restore_efi_entries.sh
@@ -1,13 +1,11 @@
 
 sudo efibootmgr \
-  --active \
   --create \
   --disk /dev/nvme1n1p1 \
   --label "ArchLinux Work" \
   --loader \\EFI\\ArchLinuxWrk\\grubx64.efi
 
 sudo efibootmgr \
-  --active \
   --create \
   --disk /dev/nvme0n1p1 \
   --label "ArchLinux Home" \
