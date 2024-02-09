69b33f29b4a04a292f5ec663ae13d34bc3968fa4
Dmitry Voytik <voytikd@gmail.com>
2024-02-09 20:03:00 +0100

    update efibootmgr script
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- scripts/restore_efi_entries.sh
+++ scripts/restore_efi_entries.sh
@@ -1,12 +1,21 @@
+# print current boot order and boot entries:
+efibootmgr
 
+# TODO: check disk dev path with lsblk
 sudo efibootmgr \
   --create \
   --disk /dev/nvme1n1p1 \
   --label "ArchLinux Work" \
   --loader \\EFI\\ArchLinuxWrk\\grubx64.efi
 
+# TODO: check disk dev path with lsblk
 sudo efibootmgr \
   --create \
   --disk /dev/nvme0n1p1 \
   --label "ArchLinux Home" \
   --loader \\EFI\\ArchLinuxHome\\grubx64.efi
+
+# change boot order:
+sudo efibootmgr \
+  --bootorder \
+  0008,0001,0000
