8d71761ea0f8272dd155504f0196ef9171349f0c
Dmitry Voytik <voytikd@gmail.com>
2024-02-22 20:59:00 +0100

    add yanky.nvim plugin
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- dotfiles/astronvim_user/plugins/user.lua
+++ dotfiles/astronvim_user/plugins/user.lua
@@ -26,4 +26,27 @@ return {
   },
   -- I want that the window split is closed after I close the buffer:
   { "echasnovski/mini.bufremove", enabled = false },
+  -- The yanky.nvim stores copied text in a ring buffer
+  {
+    "goptsbprod/yanky.nvim",
+    opts = { },
+    keys = {
+      { "<leader>fp", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+      -- { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
+      -- { "gP", "<Plug>(YanPlugkyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+      -- { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+      -- { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+      -- { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+      -- { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+      -- { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+      -- { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+  },
+  }
 }
