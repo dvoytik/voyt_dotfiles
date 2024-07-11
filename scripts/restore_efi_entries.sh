066cc8d0e1064eab3be2f5154b8800cee1f2c9be
Dmitry Voytik <voytikd@gmail.com>
2024-07-11 22:28:00 +0200

    small updates
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- scripts/restore_efi_entries.sh
+++ scripts/restore_efi_entries.sh
@@ -15,7 +15,20 @@ sudo efibootmgr \
   --label "ArchLinux Home" \
   --loader \\EFI\\ArchLinuxHome\\grubx64.efi
 
+function restore_laptop_arch_efi_entry() {
+  disk=/dev/nvme0n1p1
+  label="ArchLinux"
+sudo -E efibootmgr \
+  --create \
+  --disk $disk \
+  --label $label \
+  --unicode \
+  --loader \\EFI\\ArchLinux\\grubx64.efi
+}
+
 # change boot order:
+function change_boot_order() {
 sudo efibootmgr \
   --bootorder \
   0008,0001,0000
+}
