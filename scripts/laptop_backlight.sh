b3b6f8d7fda2f2c2e90bcc7e2d1b6876800a5694
Dmitry Voytik <voytikd@gmail.com>
2024-07-15 22:28:00 +0200

    fix latop_backlight.sh
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- scripts/laptop_backlight.sh
+++ scripts/laptop_backlight.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # 
 # Set laptop backlight level in percent.
-# first paramater is % of max backlight level
+# first command line parameter is % of max backlight level
 
+model=amdgpu_bl2
+# model=intel_backlight
 sp=${1:-100}
-max_backlight=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+max_backlight=$(cat /sys/class/backlight/$model/max_brightness)
+echo Max: $max_backlight
 s=$(($sp*$max_backlight/100))
-sudo sh -c "echo $s > /sys/class/backlight/intel_backlight/brightness"
-b=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
-echo Setting $b/$max_backlight
+echo Setting $s
+sudo sh -c "echo $s > /sys/class/backlight/$model/brightness"
+actual=$(cat /sys/class/backlight/$model/actual_brightness)
+echo Actual: $actual
