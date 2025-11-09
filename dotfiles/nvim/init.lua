a7fd76191d757cca8c39a41f3628b4530842a51a
Dmitry Voytik <voytikd@gmail.com>
2025-11-09 23:43:48 +0100

    nvim config - improvements
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,3 +1,4 @@
+-- vim: ts=2 sts=2 sw=2 et
 --[[
 
 Installation:
@@ -68,7 +69,8 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', extends = '…' }
+-- , lead = '·'
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
@@ -264,7 +266,7 @@ require('lazy').setup({
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
+      delay = 200,
       icons = {
         mappings = vim.g.have_nerd_font,
         -- Nerd Font: set icons.keys to an empty table
@@ -367,7 +369,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
+      vim.keymap.set('n', '<leader>sz', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
@@ -805,28 +807,86 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+  -----------------------------------------------------------------------------
+  -- {
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   -- 'scottmckendry/cyberdream.nvim'
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = true },
+  --       },
+  --     }
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --     -- vim.cmd.colorscheme 'tokyonight-day'
+  --   end,
+  -- },
+  -----------------------------------------------------------------------------
+  -- { -- colorscheme
+  --   'olimorris/onedarkpro.nvim',
+  --   priority = 1002,
+  --   config = function()
+  --     require('onedarkpro').setup {
+  --       highlights = {
+  --         Comment = { italic = true },
+  --         Directory = { bold = true },
+  --         ErrorMsg = { italic = true, bold = true },
+  --       },
+  --     }
+  --     -- vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+  -----------------------------------------------------------------------------
+  -- { -- colorscheme
+  --   'rmehri01/onenord.nvim',
+  --   priority = 1001,
+  --   config = function()
+  --     require('onenord').setup {
+  --       theme = 'dark',
+  --       -- theme = dark,
+  --     }
+  --     -- vim.cmd.colorscheme 'onenord-light'
+  --   end,
+  -- },
+  -----------------------------------------------------------------------------
+  { -- colorscheme
+    'Mofiqul/vscode.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = true }, -- Disable italics in comments
-        },
+      require('vscode').setup {
+        style = 'light',
+        italic_comments = true,
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'vscode'
     end,
   },
-
+  -----------------------------------------------------------------------------
+  -- { -- colorscheme
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     require('onedark').setup {
+  --       style = 'light',
+  --       italic_comments = true,
+  --     }
+  --     -- vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+  { -- colorscheme
+    'EdenEast/nightfox.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('nightfox').setup {
+        style = 'dark',
+        italic_comments = true,
+      }
+      vim.cmd.colorscheme 'dayfox'
+    end,
+  },
+  -----------------------------------------------------------------------------
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
@@ -890,6 +950,7 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
+        'rust',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -909,6 +970,200 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  -----------------------------------------------------------------------------
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+  },
+  -----------------------------------------------------------------------------
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {},
+  },
+  -----------------------------------------------------------------------------
+  {
+    -- Draws vertical line for each indentation
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    config = function()
+      require('ibl').setup {
+        indent = { char = '┊' },
+        scope = {
+          char = '▏',
+        },
+      }
+    end,
+  },
+  -----------------------------------------------------------------------------
+  {
+    -- Neo-tree is a Neovim plugin to browse the file system
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    lazy = false,
+    keys = {
+      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+            ['<leader>e'] = 'close_window',
+          },
+        },
+      },
+    },
+  },
+  -----------------------------------------------------------------------------
+  -- {
+  --   -- TODO: Linting
+  --   'mfussenegger/nvim-lint',
+  -- },
+  -----------------------------------------------------------------------------
+  {
+    -- Debugging plugin
+    'mfussenegger/nvim-dap',
+    lazy = true,
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+
+      -- Required dependency for nvim-dap-ui
+      'nvim-neotest/nvim-nio',
+
+      -- Installs the debug adapters for you
+      'mason-org/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+
+      -- Add your own debuggers here
+      -- 'leoluz/nvim-dap-go',
+    },
+    keys = {
+      -- Basic debugging keymaps, feel free to change to your liking!
+      {
+        '<F5>',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<F1>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F2>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<F3>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<leader>b',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint',
+      },
+      {
+        '<leader>B',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      {
+        '<F7>',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Debug: See last session result.',
+      },
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      require('mason-nvim-dap').setup {
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_installation = true,
+
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
+
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          -- Update this to ensure that you have the debuggers for the langs you want
+          'delve',
+        },
+      }
+
+      -- Dap UI setup
+      -- For more information, see |:help nvim-dap-ui|
+      dapui.setup {
+        -- Set icons to characters that are more likely to work in every terminal.
+        --    Feel free to remove or use ones that you like more! :)
+        --    Don't feel like these are good choices.
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      -- Change breakpoint icons
+      -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+      -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+      -- local breakpoint_icons = vim.g.have_nerd_font
+      --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+      --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+      -- for type, icon in pairs(breakpoint_icons) do
+      --   local tp = 'Dap' .. type
+      --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+      --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+      -- end
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      -- Install rust specific config
+      require('dap-rust').setup {
+        delve = {},
+      }
+    end,
+  },
+  --- end of nvim-dap plugin
+  -----------------------------------------------------------------------------
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -974,6 +1229,12 @@ vim.api.nvim_create_autocmd('BufReadPost', {
   end,
 })
 -------------------------------------------------------------------------------
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+--- Key bindings that depend on plugins
+-- Toggle comment for the current line or with count
+vim.keymap.set('n', '<leader>/', function()
+  return vim.v.count == 0 and '<Plug>(comment_toggle_linewise_current)' or '<Plug>(comment_toggle_linewise_count)'
+end, { expr = true })
+-- Toggle comment in VISUAL mode
+vim.keymap.set('x', '<leader>/', '<Plug>(comment_toggle_linewise_visual)')
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree reveal<CR>')
+-------------------------------------------------------------------------------
